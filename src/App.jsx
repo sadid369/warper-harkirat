152b5c01e67349eb1fb5be714f3a821738bba009
sadid369 <sadid.jones@gmail.com>
2024-12-08 00:10:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <CardWarper>
+        hi There
+        <div
+          style={{
+            border: "2px solid Black",
+            padding: "10px",
+            borderRadius: "10px",
+            marginTop: "10px",
+          }}
+        >
+          hello
+        </div>
+      </CardWarper>
+    </>
+  );
+}
+
+function CardWarper({ children }) {
+  return (
+    <div
+      style={{
+        border: "2px solid Black",
+        padding: "10px",
+        borderRadius: "10px",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default App;
