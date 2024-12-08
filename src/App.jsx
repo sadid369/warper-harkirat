134f09c6d4e6a79bbeeb9277a7e3d4eed3038c70
sadid369 <sadid.jones@gmail.com>
2024-12-08 17:56:56 +0600

    week 6.2 @1:50

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,42 @@
-import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  // const [todos, setTodos] = useState([]);
+  const [todoNumber, setTodoNumber] = useState(1);
+
+  useEffect(() => {
+    //   fetch("https://jsonplaceholder.typicode.com/todos")
+    //     .then((res) => res.json())
+    //     .then((data) => setTodos(data));
+    console.log("called");
+    // axios
+    //   .get("https://jsonplaceholder.typicode.com/todos/1")
+    //   .then((res) => setTodos(res.data));
+  }, []);
   return (
-    //
     <>
-      <CardWarper>
-        hi There
-        <div
-          style={{
-            border: "2px solid Black",
-            padding: "10px",
-            borderRadius: "10px",
-            marginTop: "10px",
-          }}
-        >
-          hello
-        </div>
-      </CardWarper>
+      {/* {todos.map((item) => {
+        return <div key={item.id}>{item.title}</div>;
+      })} */}
+      {/* {todos.title} */}
+      <button onClick={() => setTodoNumber(2)}>1</button>
+      <button onClick={() => setTodoNumber(3)}>2</button>
+      <button onClick={() => setTodoNumber(4)}>3</button>
+      <button onClick={() => setTodoNumber(5)}>4</button>
+      <Todos id={todoNumber} />
     </>
   );
 }
+function Todos({ id }) {
+  const [todos, setTodos] = useState({});
 
-function CardWarper({ children }) {
-  return (
-    <div
-      style={{
-        border: "2px solid Black",
-        padding: "10px",
-        borderRadius: "10px",
-      }}
-    >
-      {children}
-    </div>
-  );
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then((res) => setTodos(res.data));
+  }, [id]);
+  return <>{todos.title}</>;
 }
 
 export default App;
